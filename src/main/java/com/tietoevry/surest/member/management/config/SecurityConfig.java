f933227799442460bb4fce814187a1bf275432bb
Balakrishna P V <131838811+BalakrishnaPV08@users.noreply.github.com>
2025-12-29 16:58:59 +0530

    auth end point fix

--- src/main/java/com/tietoevry/surest/member/management/config/SecurityConfig.java
+++ src/main/java/com/tietoevry/surest/member/management/config/SecurityConfig.java
@@ -49,8 +49,7 @@ public class SecurityConfig {
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
-                                "/auth/**",
-                                "/actuator/**",
+                                "/api/v1/auth/**",
                                 "/swagger-ui.html",
                                 "/swagger-ui/**",
                                 "/v3/api-docs/**"
